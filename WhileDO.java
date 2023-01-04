f7d7eade5ce7258c7b9142fd457dfbae808ae128
Fatih Sancak <fsancak@gmail.com>
2023-01-04 18:49:39 +0300

    While Do Döngüsüne Örnek

---
+++ WhileDO.java
@@ -0,0 +1,11 @@
+
+
+public class WhileDO {
+    public static void main(String[] args) {
+        char letter = 'A';
+        while (letter <= 'Z') {
+            System.out.print( letter + " ");
+            letter++;
+        }
+    }
+}

6fdd32bcaf0992b39affaa4f65bd22c0dd4e1746
Fatih Sancak <fsancak@gmail.com>
2023-01-05 00:49:23 +0300

    01.January.2023

---
+++ src/day02_variables/C01_Variables.java
@@ -0,0 +1,25 @@
+package day02_variables;
+   public class C01_Variables{
+        public static void main(String[] args) {
+            // Data türü    Variable İsmi     assigmnet sign       variable değeri
+                int             not                 =                   60;
+
+                int not2        =     70;
+            //  variable             değer
+
+            // Java önce değeri hesaplar sonra assign işlenmi yapar
+                not2 =90;
+
+            // yeni bir int variable oluşturalım değeri ilk iki variable'in ortalamaası olsun
+
+                int ort = (not + not2) / 2;
+
+            // "" içine yazılaan her sey metindir, Java " " içinde ne görürse o şekilde yazdırır.
+            // eğer bir variable'in değerini yazdırıyorsanız
+            // " " olmadan variable ismini yazmalısınız
+
+                System.out.println(ort);
+
+
+        }
+    }

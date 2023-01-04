9a5de4463cc9d76c342c46dcd4c2fbdd47bb45b3
Fatih Sancak <fsancak@gmail.com>
2023-01-05 00:49:01 +0300

    01.January.2023

---
+++ src/day01_variables/C01_HelloWorld.java
@@ -0,0 +1,10 @@
+package day01_variables;
+
+public class C01_HelloWorld
+{
+    // main methodun blok halinde ekrana yazdırmak için main
+    // System.out.println("") için sadece sout yazıp TAB yapıldığında bu methodlar otomatik olarak yazılacaktır.
+    public static void main(String[] args) {
+        System.out.println("Hello Java. Hello World");
+    }
+}

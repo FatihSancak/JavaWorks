8f705cac4e8e726fcb6920f6854129c8629d00bf
Fatih Sancak <fsancak@gmail.com>
2023-01-04 18:44:50 +0300

    Do While Döngüsüne Örnek

---
+++ Deneme1.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Deneme1 {
+    public static void main(String[] args) {
+        System.out.println("Merhaba Dunya");
+        System.out.println("Bir değerlendirme notu giriniz");
+
+        Scanner scan = new Scanner(System.in);
+
+        int sayi = scan.nextInt();
+
+        do{
+            sayi++;
+            System.out.println(sayi);
+        }while (sayi<10);
+
+    }
+}

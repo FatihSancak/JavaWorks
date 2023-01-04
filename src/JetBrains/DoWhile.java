6710ff5972bf2867588ccd0630026cedc5b3beef
Fatih Sancak <fsancak@gmail.com>
2023-01-05 00:58:12 +0300

    05.January.2023

--- src/JetBrains/DoWhile.java
+++ src/JetBrains/DoWhile.java
@@ -1,4 +1,4 @@
-package src;
+package src.JetBrains;
 
 import java.util.Scanner;
 
--- src/JetBrains/WhileDO.java
+++ src/JetBrains/WhileDO.java
@@ -1,4 +1,4 @@
-package src;
+package src.JetBrains;
 
 public class WhileDO {
     public static void main(String[] args) {

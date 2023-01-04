5591ac1f02f3db5c7a5036a7f249739d27481d27
Fatih Sancak <fsancak@gmail.com>
2023-01-05 00:55:15 +0300

    05.January.2023

---
+++ src/day07_ifStatements/C05_ifElseStatements.java
@@ -0,0 +1,19 @@
+package day07_ifStatements;
+
+import java.util.Scanner;
+
+public class C05_ifElseStatements {
+    public static void main(String[] args) {
+        /*
+        Kulllanıcıdan bir karakter girmesini isteyin ve
+        girilen karakterlerin harf olup olmadığını yazdırın.
+         */
+
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Lütfen bir karakter giriniiz : ");
+      //  Char harf = scan.next().charAt(0);
+      //  if (harf>='a' && harf <='z') || (harf >='A' && harf<='Z'){
+
+      //  }
+    }
+}

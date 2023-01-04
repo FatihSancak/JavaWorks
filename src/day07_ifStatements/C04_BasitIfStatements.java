1eb728356c4d19f4074c45036d7975ac0b76b316
Fatih Sancak <fsancak@gmail.com>
2023-01-05 00:55:02 +0300

    04.January.2023

---
+++ src/day07_ifStatements/C04_BasitIfStatements.java
@@ -0,0 +1,37 @@
+package day07_ifStatements;
+
+import java.util.Scanner;
+
+public class C04_BasitIfStatements {
+    public static void main(String[] args) {
+        /*
+        Kullanıcıdan gün ismini alın.
+        hafta içi veya hafta sonu olduğunu yazdırın
+        örnek :
+        input: Salı output: "Hafta içi"
+        input:Pazar output: "Hafta sonu"
+
+        *** String için equels metodunu kullanın
+         */
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Lütfen bir gün giriniz : ");
+        String girilenGun = scan.next();
+
+        if(girilenGun.equals("pazar") || girilenGun.equals("cumartesi")){
+            System.out.println("Girilen Gün Hafta Sonu");
+        }
+
+        if(girilenGun.equals("pazartesi") || girilenGun.equals("salı")
+                || girilenGun.equals("çarşamba") || girilenGun.equals("perşembe")
+                || girilenGun.equals("cumar")){
+            System.out.println("Hafta İçi");
+        }
+
+        if(!(girilenGun.equals("pazartesi") || girilenGun.equals("salı")
+                || girilenGun.equals("çarşamba") || girilenGun.equals("perşembe")
+                || girilenGun.equals("cuma"))){
+            System.out.println("Geçerli Bir Gün Giriniz");
+
+        }
+    }
+}

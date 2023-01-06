edad6fd9c3ee4e6cb6b4e6e03a7d3108586130a7
Fatih Sancak <fsancak@gmail.com>
2023-01-06 03:12:20 +0300

    06.01.2023

---
+++ src/day08_ifSatatements/C02_IfElseStatements.java
@@ -0,0 +1,39 @@
+package src.day08_ifSatatements;
+
+import java.util.Scanner;
+
+public class C02_IfElseStatements {
+    public static void main(String[] args) {
+        /*
+        SORU
+        ***********************
+        Kullanıcıdan 100 üzerinde notunu isteyin
+        Not'u harf sisteme cevirip yazdırın;
+        50'den küçükse "D",
+        50'ye eşitv eya büyük 60'dan küçük  arası "C",
+        60'ye eşitv eya büyük 50'dan küçük  arası  "B",
+        80'nin üzeri ise "A"
+        geçersiz not girildiğinde uyarı verilsin
+        ***********************
+         */
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Lütfen bir gün ismi giriniz :  ");
+        double not = scan.nextDouble();
+
+        if (not<0 || not>100) {
+            System.out.println("Geçerli bir puan giriniz.");
+        }
+        else if(not<50){
+            System.out.println("Notunuz : D ");
+        }
+        else if(not<60){
+            System.out.println("Notunuz : C");
+        }
+        else if(not<80){
+            System.out.println("Notunuz : B");
+        }
+        else{
+            System.out.println("Notunuz : A");
+        }
+    }
+}
---
+++ src/day08_ifSatatements/C03_NestedIfElse.java
@@ -0,0 +1,44 @@
+package src.day08_ifSatatements;
+
+import java.util.Scanner;
+
+public class C03_NestedIfElse {
+    public static void main(String[] args) {
+         /*
+         emeklilik kontorlü yapan bir program yazınız
+         cinsiyet olarak E (erkek) veya K (kadın) değişkenleri kabul edilsin
+         farklı bir harf ve sembol girilise hata mesajı versin
+
+         emeklilik için kadınlarda yaş sınırı 60
+         erkeklerde 65 olsun
+         negatif yas veya 80'den buyuk yas girilirse hata mesajı versin
+          */
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Emeklilik Durumunuzu görmek için istenene bilgileri giriniz");
+        System.out.print("Lütfen Cinsiyetinizi Giriniz : ");
+        char cinsiyet = scan.next().toUpperCase().charAt(0);
+        System.out.print("Lütfen Yaşınızı Giriniz : ");
+        double yas = scan.nextDouble();
+
+        if (cinsiyet =='E'){
+            if (yas<0 || yas > 80 ){
+                System.out.println("Emeklilik yaşı aralığında değilsiniz.");
+            } else if (yas<65) {
+                System.out.println("Emekli olamazsınız");
+            }else{
+                System.out.println("Emekli olabilirsiniz");
+            }
+        }else if (cinsiyet=='K'){
+            if (yas<0 || yas > 60 ){
+                System.out.println("Emeklilik yaşı aralığında değilsiniz.");
+            } else if (yas<60) {
+                System.out.println("Emekli olamazsınız");
+            }else{
+                System.out.println("Emekli olabilirsiniz");
+            }
+        }else {
+            System.out.println("Lütfen geçerli bir cinsiyet girişi yapınız.");
+        }
+    }
+}

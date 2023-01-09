dbe51bd26324b888143f7e258f559f5e09d97f60
Fatih Sancak <fsancak@gmail.com>
2023-01-10 00:47:55 +0300

    10.01.2023

---
+++ src/day13_MethodCreation/C04_MethodCreation.java
@@ -0,0 +1,31 @@
+package src.day13_MethodCreation;
+
+public class C04_MethodCreation {
+    public static void main(String[] args) {
+        // input olarak iki int'ı toplayıp
+        // sonucunu yazdıran
+        // bir method oluşturun
+        int input1 = 180;
+        int input2 = 20;
+
+        /*
+        Method 4 adımda oluşturulur.
+        1. adım method call
+        2. adım argument eklenmesi gerekiyorsa ekleyelim
+           eğer method'un return type void'den farklı olacaksa
+           bir variable oluşturup, method call'u assign edelim
+        */
+
+        topla(input1,input2);
+    }
+
+    public static void topla(int input1, int input2) {
+        /*
+        3. adım method deklarasyonunda değiştirilmesi gereken yerleir değiştir.
+        (access modifier, return type vb)
+        4. eğer return type void dışında ise
+        return keywordu ve dönecek değeri hesaplamalıyız
+         */
+        System.out.println("Girilen iki sayının toplamı : "+(input1 + input2));
+    }
+}

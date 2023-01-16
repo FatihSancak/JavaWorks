e979f999332428ca04f4d407fb7bbaad7d5770c6
Fatih SANCAK <fsancak@gmail.com>
2023-01-17 00:41:00 +0300

    16-01-2023

--- src/day16_forLoop/C06_NestedForLoop.java
+++ src/day16_forLoop/C06_NestedForLoop.java
@@ -28,6 +28,5 @@ public class C06_NestedForLoop {
             }
             System.out.println("");
         }
-
     }
 }
---
+++ src/day17_nestedForLoop/C01_ForLoop.java
@@ -0,0 +1,34 @@
+package src.day17_nestedForLoop;
+
+public class C01_ForLoop {
+    /*
+    * verilen String'deki kullanılan harfleri
+    * tekrarsız olarak yazdırıp
+    * kelimede kullanılan farklı harf sayısını veren bir method yazınız.
+    */
+    public static void main(String[] args) {
+        String input ="Java güzeldir";
+
+        tekrarsizYap(input);
+
+    }
+    public static void tekrarsizYap(String input) {
+        String benzersizInput="";
+
+        String islenecekKelime = input.replaceAll("\\W","");// boşluklar kaldırıldı
+        System.out.print(islenecekKelime.substring(0,1)); // sıfırıncı indexdeki harfi al
+        benzersizInput += islenecekKelime.substring(0,1); // sıfırıncı indexdeki harfi al
+
+        for (int i = 1; i < islenecekKelime.length(); i++) {
+
+            if(!benzersizInput.contains(islenecekKelime.substring(i,i+1))){
+                System.out.print(", "+islenecekKelime.substring(i,i+1));
+                benzersizInput+=islenecekKelime.substring(i,i+1);
+            }
+        }
+        System.out.println("");
+        System.out.println("input = " + input);
+        System.out.println("benzersizInput = " + benzersizInput);
+        System.out.println("islenecekKelime = " + islenecekKelime);
+    }
+}

3fd868972c40ced1f14061058ee65169782588dc
Fatih SANCAK <fsancak@gmail.com>
2023-02-03 18:57:09 +0300

    03/Feb./2023

---
+++ src/day23_arrayList/C04_remove.java
@@ -0,0 +1,39 @@
+package src.day23_arrayList;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class C04_remove {
+    public static void main(String[] args) {
+        List<String> urunler = new ArrayList<>();
+
+        urunler.add("Nutella");
+        urunler.add("Ýkram");
+        urunler.add("Çekirdek");
+        urunler.add("Çay");
+        /*
+        remove() iki þekilde çalýþýr
+        1- objeyi yazýp silmesini istersek bize boolean sonuc doner true/false
+        2- index girersek o index'deki elemani siler ve bize silinen elemaný döndürür.
+         */
+
+        System.out.println(urunler); // [Nutella, Ýkram, Çekirdek, Çay]
+
+        System.out.println(urunler.remove("Ýkram")); // true
+
+        System.out.println(urunler); // [Nutella, Çekirdek, Çay]
+
+        System.out.println(urunler.remove("Hobby")); // false
+
+        System.out.println(urunler); // [Nutella, Çekirdek, Çay]
+
+        // ikinci yöntem
+
+        urunler.remove(1); // 1. indexi
+        System.out.println(urunler);
+
+        // olmayan bir indexi silmeye çalýþýrsak
+        // urunler.remove(3);
+        // IndexOutOfBoundsException hatasý verir
+    }
+}

5e994083783b68e9d8881ee8abd0c8a7981ab9a9
Fatih SANCAK <fsancak@gmail.com>
2023-03-20 18:28:19 +0300

    20/Mar./2023

---
+++ src/day38_Exceptions/C00_Repeat.java
@@ -0,0 +1,12 @@
+package src.day38_Exceptions;
+
+public class C00_Repeat
+{
+/*
+1 . Pollymorphism : çok þekillik, Java'da ayný isimdeki bir method'un farklý farklý özellikler gösterebilmesidir.
+2. Ýki ihtimal vardýr::
+    1. Overloading: Ayný class'da ve ayný isimde olup farklý method signature'ýna sahip birden fazla method oluþturabilir ve böylece ayný isimdeki farklý farklý özellikler üretebiliriz.
+    2. Overridding: Overridding sadece parent v child class'larda olur. Amacý parent class'daki bir method'un ayný isim ve signature'a sahip farklý özellik üretmesini saðlamaktýr.
+    Pollymorphism = Overloading + Overridding
+ */
+}
